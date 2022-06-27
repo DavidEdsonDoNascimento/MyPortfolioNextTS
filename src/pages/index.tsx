2e13359774f49b89896138436685cc2c56b0cd2a
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2022-06-26 21:06:29 -0300

    FEAT: #main - init Home page and adjust settings tsconfig.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Container } from '../styles/pages/Home'
+import { 
+  Container, 
+  Header 
+} from '../styles/pages/Home'
 
 const Home: NextPage = () => {
   return (
@@ -8,6 +11,20 @@ const Home: NextPage = () => {
        <Head>
         <title>David Edson | JS Dev</title>
       </Head>
+      <Header>
+        <div className='wrapper'>
+          <div>
+            <h3>Buenas!</h3>
+            <span>Me chamo <a href="https://www.linkedin.com/in/david-edson-05989a177" target="_blank" rel="noreferrer"><b>David Edson</b></a>,</span>
+            <p>sou Desenvolvedor FullStack JavaScript.</p>
+          </div>
+          <img
+            src={'/svgs/logo-big.svg'}
+            alt='Logotipo com as letras Dev Full'
+            className='logo'
+          />
+        </div>
+      </Header>
     </Container>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Container = styled.div`
   width: 100%;
@@ -7,7 +7,7 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-`;
+`
 
 const Header = styled.header`
   width: 100%;
@@ -110,7 +110,7 @@ const Header = styled.header`
       }
     }
   }
-`;
+`
 
 const Main = styled.main`
   width: 100%;
@@ -118,7 +118,7 @@ const Main = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-`;
+`
 
 const Footer = styled.footer`
   width: 100%;
@@ -186,7 +186,7 @@ const Footer = styled.footer`
       }
     }
   }
-`;
+`
 
 export {
   Container,
