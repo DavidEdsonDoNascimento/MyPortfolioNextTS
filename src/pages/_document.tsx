a03bbeaedeb2cde0930bb799bc75f1a22d5d7f23
David Edson do Nascimento <david.contato.tec@gmail.com>
2022-08-07 12:45:21 -0300

    CHORE: #main - add lang pt-BR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
           <meta charSet="UTF-8" />
           <meta name="description" content="Desenvolvedor JavaScript Full Stack sempre buscando por novos desafios e espalhando o conhecimento" />
