a6939d3cb2f8459985e0d54e57c8b8ac6cfa778f
David Edson do Nascimento <david.contato.tec@gmail.com>
2022-08-09 13:55:48 -0300

    CHORE: #main - changed meta CharSet to httpEquiv

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <meta charSet="UTF-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="description" content="Desenvolvedor JavaScript Full Stack sempre buscando por novos desafios e espalhando o conhecimento" />
           <meta name="keywords" content="Desenvolvedor,Full,Stack,pleno,JavaScript,React,Next,ReactJS,NextJS,Programador,tecnologia,codigos,escultor,digital,clean code,Dev JS,David Edson" />
           <meta name="author" content="David Edson Do Nascimento" />
-          <meta name="robots" content="index,follow"/>
-          <meta name="googlebot" content="index,follow"/>
+          <meta name="robots" content="index,follow" />
+          <meta name="googlebot" content="index,follow" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&display=swap" rel="stylesheet" />
