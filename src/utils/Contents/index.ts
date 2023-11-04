fcae17568c09a69ae2d603ea9c0f2347c144770f
David Edson do Nascimento <david.contato.tec@gmail.com>
2023-11-03 23:30:15 -0300

    FEAT: adjust in about text and skills

--- src/utils/Contents/index.ts
+++ src/utils/Contents/index.ts
@@ -4,10 +4,10 @@ export abstract class Content {
 
   static about(): string {
     return `Meu nome é David Edson, tenho ${this.MY_AGE} anos, sou Desenvolvedor Full Stack, trabalho com amor e cuidado nos códigos,
-    Desde o ano de 2019 estou concentrado em me especializar nas tecnologias que permeiam o JavaScript, estudando todos os dias, sempre Node, JavaScript, Typescript, ReactJS, NextJS, Angular, ou regras de negocios, buscando seguir as boas práticas, padrões de projetos, sempre procurando acordar todos os dias à procura de alguma nova coisa que possa ajudar alguém no futuro, seguindo a ideologia do 1% todos os dias, ajudando assim clientes e empresas a alcançar a excelencia.
+    Desde o ano de 2019 estou concentrado em me especializar nas tecnologias que permeiam o JavaScript, estudando todos os dias, sempre Node, JavaScript, Typescript, ReactJS, React Native, NextJS, Angular, ou regras de negocios, buscando seguir as boas práticas, padrões de projetos, sempre procurando acordar todos os dias à procura de alguma nova coisa que possa ajudar alguém no futuro, seguindo a ideologia do 1% todos os dias, ajudando assim clientes e empresas a alcançar a excelencia.
 
     Tenho proficiencia com:
-    NodeJS, Typescript, Javascript, ReactJS, NextJS, SQL, Banco de dados, SCRUM, HTML e CSS..`;
+    NodeJS, Typescript, Javascript, ReactJS, React Native, NextJS, SQL, Banco de dados, SCRUM, HTML e CSS..`;
   }
 
   static skills(): string {
