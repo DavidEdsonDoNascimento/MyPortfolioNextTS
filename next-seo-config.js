a3f8342a2d8b668f5413b308b534413b3a6cadda
DavidEdsonDoNascimento <david.nascimento@videosoft.com.br>
2024-03-11 09:53:32 -0300

    FIX: main - adjunst href link

--- next-seo-config.js
+++ next-seo-config.js
@@ -1,25 +1,25 @@
-const title = 'Escultor Digital - Desenvolvedor Javascript';
+const title = 'David Edson - Desenvolvedor Javascript';
 const description = 'Desenvolvedor JavaScript Full Stack sempre buscando por novos desafios e espalhando o conhecimento';
 
 export const SEO = {
   title,
   description,
-  canonical: 'https://escultordigital.com.br',
+  canonical: 'https://davidedson.com.br',
   openGraph: {
     type: 'website',
     locale: 'pt_BR',
-    url: 'https://escultordigital.com.br',
+    url: 'https://davidedson.com.br',
     title,
     description,
     images: [
       {
-        url: 'https://www.escultordigital.com.br/imgs/men.png',
+        url: 'https://www.davidedson.com.br/imgs/men.png',
         alt: title,
         width: 1280,
         height: 884,
       },
       {
-        url: 'https://www.escultordigital.com.br/_next/image?url=%2Fimgs%2Feu-e-a-mona-chopera.jpeg&w=750&q=75',
+        url: 'https://www.davidedson.com.br/_next/image?url=%2Fimgs%2Feu-e-a-mona-chopera.jpeg&w=750&q=75',
         alt: 'David Edson image',
         width: 720,
         height: 719,
