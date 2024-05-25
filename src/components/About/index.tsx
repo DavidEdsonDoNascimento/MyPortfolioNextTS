e4186c37e442ec22047f82326a9959162061dc5b
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-05-25 12:18:55 -0300

    added iframe in about me

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,34 +1,30 @@
-import { Content } from "@utils/Contents";
-import { SocialNetworks } from "@utils/SocialNetworks";
-import { Container } from "./styles";
+import { Content } from '@utils/Contents';
+import { SocialNetworks } from '@utils/SocialNetworks';
+import { Container } from './styles';
 
 export const About = () => {
   return (
     <Container>
-      <div id="sobre" className="wrapper">
-        <div className="content">
+      <div id='sobre' className='wrapper'>
+        <div className='content'>
           <h2>Sobre</h2>
           <p>{Content.about()}</p>
-          <div className="buttons">
-            <a href={SocialNetworks.linkedin.link} className="button-link">
-              LinkedIn
-            </a>
-            <a href={SocialNetworks.github.link} className="button-link">
-              Github
-            </a>
+          <div className='buttons'>
+            <a href={SocialNetworks.linkedin.link} className='button-link'>LinkedIn</a>
+            <a href={SocialNetworks.github.link} className='button-link'>Github</a>
           </div>
         </div>
-        <div className="bg-perfil">
+        <div className='bg-perfil'>
           <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/6mRzghtdwLg?si=MWS4L1yEiUtcj2Hq"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
+            width='560'
+            height='315'
+            src='https://www.youtube.com/embed/6mRzghtdwLg?si=MWS4L1yEiUtcj2Hq'
+            title='YouTube video player'
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            referrerPolicy='strict-origin-when-cross-origin'
             allowFullScreen
-          ></iframe>
+            ></iframe>
         </div>
       </div>
     </Container>
