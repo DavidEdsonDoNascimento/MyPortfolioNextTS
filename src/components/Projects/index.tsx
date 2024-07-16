8a6b812be539d43caced76cc6f0a409aa4ef7661
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-16 19:25:15 -0300

    adjust in projects section

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,6 +1,13 @@
 import { IoRocket } from 'react-icons/io5';
 import { FiLink } from 'react-icons/fi';
-import { DiCss3, DiGitBranch, DiHtml5, DiNodejs, DiWordpress } from 'react-icons/di';
+import {
+  DiCss3,
+  DiGitBranch,
+  DiHtml5,
+  DiNodejs,
+  DiReact,
+  DiWordpress,
+} from 'react-icons/di';
 import { IoLogoJavascript } from 'react-icons/io';
 import { MdHttp } from 'react-icons/md';
 
@@ -10,63 +17,93 @@ import { SiPrisma, SiTypescript } from 'react-icons/si';
 export const Projects = () => {
   return (
     <Container>
-      <div id='projetos' className='wrapper'>
-        <h2><IoRocket />Projetos</h2>
-        <div className='projects-group'>
-          <div className='projects-list'>
-            <div className='project'>
-              <h3>Camboriú aluguel</h3>
-              <p>Site de alugueis de estadias em Balneário Camboriú - SC, desenvolvido por mim.</p>
+      <div id="projetos" className="wrapper">
+        <h2>
+          <IoRocket />
+          Projetos
+        </h2>
+        <div className="projects-group">
+          <div className="projects-list">
+            <div className="project">
+              <h3>Fanfic Linkedin</h3>
+              <p>
+                Projeto que visa ser uma brincadeira com o Linkedin, feito para
+                efeitos de teste.
+              </p>
               <a
-                href="http://camboriualuguel.com.br"
-                target='_blank'
-                className='button-link'
+                href="https://fanfic-corp.vercel.app/"
+                target="_blank"
+                className="button-link"
                 rel="noreferrer"
-              ><FiLink />Website</a>
-              <div className='icons'>
-                <DiWordpress />
+              >
+                <FiLink />
+                Website
+              </a>
+              <div className="icons">
+                <DiReact />
                 <DiHtml5 />
                 <IoLogoJavascript />
                 <MdHttp />
               </div>
             </div>
-            <div className='project'>
+            <div className="project">
               <h3>API RestFull SOLID</h3>
-              <p>Uma API RestFull contém princípios SOLID, aproveitando o uso de padrões de projetos relevantes.</p>
+              <p>
+                Uma API RestFull contém princípios SOLID, aproveitando o uso de
+                padrões de projetos relevantes.
+              </p>
               <a
                 href="https://github.com/DavidEdsonDoNascimento/solid-api"
-                target='_blank'
-                className='button-link'
+                target="_blank"
+                className="button-link"
                 rel="noreferrer"
-              ><DiGitBranch />Repositório</a>
-              <div className='icons'>
+              >
+                <DiGitBranch />
+                Repositório
+              </a>
+              <div className="icons">
                 <DiNodejs />
                 <SiTypescript />
                 <IoLogoJavascript />
                 <SiPrisma />
               </div>
             </div>
-            <div className='project'>
+            <div className="project">
               <h3>Golden Raspberry Awards API</h3>
-              <p>Um desafio técnico recente, API RESTful que trabalha com uma lista de indicados e vencedores da categoria Pior Filme do Golden Raspberry Awards em formato CSV.</p>
+              <p>
+                Um desafio técnico recente, API RESTful que trabalha com uma
+                lista de indicados e vencedores da categoria Pior Filme do
+                Golden Raspberry Awards em formato CSV.
+              </p>
               <a
                 href="https://github.com/DavidEdsonDoNascimento/golden-raspberry-awards-api"
-                target='_blank'
-                className='button-link'
+                target="_blank"
+                className="button-link"
                 rel="noreferrer"
-              ><DiGitBranch />Repositório</a>
-              <div className='icons'>
+              >
+                <DiGitBranch />
+                Repositório
+              </a>
+              <div className="icons">
                 <DiNodejs />
                 <SiTypescript />
                 <IoLogoJavascript />
                 <SiPrisma />
               </div>
             </div>
-            <div className='project'>
+            <div className="project">
               <h3>Jogo do Mario</h3>
               <p>Jogo feito em javascript puro </p>
-              <a href="https://mario-jump-iota.vercel.app/" target='_blank' className='button-link' rel="noreferrer"><FiLink />Website</a>
-              <div className='icons'>
+              <a
+                href="https://mario-jump-iota.vercel.app/"
+                target="_blank"
+                className="button-link"
+                rel="noreferrer"
+              >
+                <FiLink />
+                Website
+              </a>
+              <div className="icons">
                 <DiHtml5 />
                 <IoLogoJavascript />
                 <DiCss3 />
@@ -74,7 +111,14 @@ export const Projects = () => {
             </div>
           </div>
         </div>
-        <a href="https://github.com/DavidEdsonDoNascimento?tab=repositories" target='_blank' rel="noreferrer" className='more-projects'>Conheça todos os projetos!</a>
+        <a
+          href="https://github.com/DavidEdsonDoNascimento?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+          className="more-projects"
+        >
+          Conheça todos os projetos!
+        </a>
       </div>
     </Container>
   );
