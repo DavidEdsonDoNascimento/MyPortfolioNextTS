0f598153e2da5b366da10c7e69558b4894757e3f
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-24 19:19:20 -0300

    Ajust in linkedin contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -5,35 +5,64 @@ import {
   RiLinkedinBoxFill,
   RiInstagramFill,
   RiMailFill,
-  RiGithubFill
+  RiGithubFill,
 } from 'react-icons/ri';
 import { Container } from './styles';
 
 export const Contact = () => {
   return (
     <Container>
-      <div id='contato' className='wrapper'>
-        <h2><RiWechatFill />Contato</h2>
-        <p>Precisa de um desenvolvedor?<br/>Vamos trabalhar juntos.<br/>Entre em contato:</p>
-        <div className='links'>
-          <div className='first-group'>
-            <a href="https://wa.me/5547997011323?text=Oi%20David%20vi%20seu%20portfólio%20e%20gostaria%20de%20bater%20um%20papo%20contigo!"
-              target='_blank' rel='noreferrer'>
-              <Tooltip item={<RiWhatsappFill />} text="Whatsapp" /></a>
-            <a href="https://github.com/DavidEdsonDoNascimento"
-              target='_blank' rel='noreferrer'>
-              <Tooltip item={<RiGithubFill />} text="Github" /></a>
+      <div id="contato" className="wrapper">
+        <h2>
+          <RiWechatFill />
+          Contato
+        </h2>
+        <p>
+          Precisa de um desenvolvedor?
+          <br />
+          Vamos trabalhar juntos.
+          <br />
+          Entre em contato:
+        </p>
+        <div className="links">
+          <div className="first-group">
+            <a
+              href="https://wa.me/5547997011323?text=Oi%20David%20vi%20seu%20portfólio%20e%20gostaria%20de%20bater%20um%20papo%20contigo!"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip item={<RiWhatsappFill />} text="Whatsapp" />
+            </a>
+            <a
+              href="https://github.com/DavidEdsonDoNascimento"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip item={<RiGithubFill />} text="Github" />
+            </a>
           </div>
-          <div className='second-group'>
-            <a href="https://www.linkedin.com/in/david-edson-05989a177/"
-              target='_blank' rel='noreferrer'>
-              <Tooltip item={<RiLinkedinBoxFill />} text="LinkedIn" /></a>
-            <a href="https://www.instagram.com/davidedsonnn"
-              target='_blank' rel='noreferrer'>
-              <Tooltip item={<RiInstagramFill />} text="Instagram" /></a>
-            <a href="mailto:david.contato.tec@gmail.com?subject=Oi%20David%20vi%20seu%20portfólio%20e%20gostaria%20de%20bater%20um%20papo%20contigo!"
-              target='_blank' rel='noreferrer'>
-              <Tooltip item={<RiMailFill />} text="E-mail" /></a>
+          <div className="second-group">
+            <a
+              href="https://www.linkedin.com/in/david-nascimento-05989a177/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip item={<RiLinkedinBoxFill />} text="LinkedIn" />
+            </a>
+            <a
+              href="https://www.instagram.com/davidedsonnn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip item={<RiInstagramFill />} text="Instagram" />
+            </a>
+            <a
+              href="mailto:david.contato.tec@gmail.com?subject=Oi%20David%20vi%20seu%20portfólio%20e%20gostaria%20de%20bater%20um%20papo%20contigo!"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Tooltip item={<RiMailFill />} text="E-mail" />
+            </a>
           </div>
         </div>
       </div>
